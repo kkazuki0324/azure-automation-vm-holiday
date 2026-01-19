5e189d6934391c6069f6615f8b74510d81905946
kkazuki3003 <81226567+kkazuki0324@users.noreply.github.com>
2026-01-19 18:11:49 +0900

    Update not_holidays.ps1

--- scripts/not_holidays.ps1
+++ scripts/not_holidays.ps1
@@ -10,15 +10,13 @@
 
 Write-Output "祝日判定を開始します..."
 
-# holidays_JP変数から祝日一覧を取得
 $Holidays = (Get-AutomationVariable -Name 'holidays_JP') -split ","
-
-
-# 今日の日付を取得
 $Today = Get-Date -Format "yyyy/M/d"
 
-# 今日が祝日に含まれるか判定
-$isHoliday = $Holidays.Contains($Today)
+if ($Holidays.Contains($Today)) {
+    Write-Output "今日は祝日です。処理を終了します。"
+    return
+}
 
-# 結果出力（True:祝日 / False:平日）
-Write-Output $isHoliday
+Write-Output "平日のため VM 操作を実行します"
+# Start-AzVM / Stop-AzVM
